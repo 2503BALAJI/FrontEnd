ffd6a791804cd3e4aa67e6e950cb7cfcac5ad870
Balaji borude <121490947+2503BALAJI@users.noreply.github.com>
2024-09-20 13:59:32 +0530

    fixing small bugs and issue

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -3,7 +3,7 @@ import { faBullseye, faEye } from "@fortawesome/free-solid-svg-icons";
 
 const Vision = () => {
   return (
-    <div className="bg-white w-full py-16 px-6 md:px-16 lg:px-32">
+    <div className="  bg-white w-full py-16 px-6 md:px-16 lg:px-32">
       {/* Main heading */}
       <div className="max-w-4xl mx-auto text-center">
         <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-gray-900 mb-4 overflow-hidden">
@@ -17,13 +17,13 @@ const Vision = () => {
       </div>
 
       {/* Mission and Vision Section */}
-      <div className="flex flex-col md:flex-row justify-between items-start mt-12 space-y-8 md:space-y-0 md:space-x-8">
+      <div className="flex flex-col md:flex-row justify-between items-start mt-12 space-y-8 md:space-y-0 md:space-x-8  p-10">
         {/* Our Mission */}
-        <div className="flex-1 bg-white rounded-lg shadow-lg overflow-hidden p-6 md:p-8">
+        <div className="flex-1 bg-white rounded-lg shadow-lg overflow-hidden p-6 md:p-8  transform transition-transform duration-300 hover:scale-105">
           <h3 className="text-xl md:text-2xl lg:text-3xl font-semibold mb-4 overflow-hidden text-gray-900">
             Our Mission
           </h3>
-          <div className="flex items-center mb-6">
+          <div className="flex items-center mb-6 ">
             <FontAwesomeIcon
               icon={faBullseye}
               size="2x"
@@ -38,7 +38,8 @@ const Vision = () => {
         </div>
 
         {/* Our Vision */}
-        <div className="flex-1 bg-white rounded-lg shadow-lg overflow-hidden p-6 md:p-8">
+        <div className="flex-1 bg-white rounded-lg shadow-xl overflow-hidden p-6 md:p-8 
+        transform transition-transform duration-300 hover:scale-105 ">
           <h3 className="text-xl md:text-2xl lg:text-3xl font-semibold overflow-hidden mb-4 text-gray-900">
             Our Vision
           </h3>
