0b278d516132c32c5c54862c2aa42d312762cfbe
Balaji borude <121490947+2503BALAJI@users.noreply.github.com>
2024-09-22 19:49:43 +0530

    working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
     <div className='w-11/12 mx-auto bg-red-600 h-screen'>
       {/* path wala div */}
       <div>
-        <a href=''> project</a>
-        <a href=''>Question</a>
+        <a href='#project'> project</a>
+        <a href='#question'>Question</a>
         <a href='#about'>About us </a>
       </div>
     </div>
